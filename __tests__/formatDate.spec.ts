6e6881d219c9768c6f5b80a07dd536caac6096d9
Grzegorz Nęcka <strzypior@gmail.com>
2022-04-03 17:57:29 +0200

    third test

--- __tests__/formatDate.spec.ts
+++ __tests__/formatDate.spec.ts
@@ -8,7 +8,31 @@ describe("formatCurrency", () => {
         expect(formatDate("2021-05-10")).toMatchObject({ year: 2021, month: 5, day: 10 });
     });
 
-    // it("returns date object for 2021-13-10 ", () => {
-    //     expect(formatDate("2021-13-10")).toBe(null);
-    // });
+    it("returns null for incorrect month above range: 2021-00-10 ", () => {
+        expect(formatDate("2021-00-10")).toBe(null);
+    });
+
+    it("returns null for incorrect month below  range: 2021-13-10 ", () => {
+        expect(formatDate("2021-00-10")).toBe(null);
+    });
+
+    it("returns null for incorrect day above range: 2021-10-32 ", () => {
+        expect(formatDate("2021-10-32")).toBe(null);
+    });
+
+    it("returns null for incorrect day below range: 2021-10-00 ", () => {
+        expect(formatDate("2021-10-00")).toBe(null);
+    });
+
+    it("returns null for incorrect yar below range: 1899-12-31", () => {
+        expect(formatDate("1899-12-31")).toBe(null);
+    });
+
+    it("returns null for incorrect yar above range: 2101-01-01", () => {
+        expect(formatDate("2101-01-01")).toBe(null);
+    });
+
+    it("returns error for wrong day date", () => {
+        expect(() => formatDate("2000-02-30")).toThrow("you are using the wrong day");
+    });
 });
--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,11 +1,5 @@
 export const formatDate = (dateToFormat: string): object | null => {
-    const [y, m, d] = dateToFormat.split("-");
-
-    const n = (num: string): number => Number.parseInt(num);
-
-    const year = n(y);
-    const month = n(m);
-    const day = n(d);
+    const [year, month, day] = dateToFormat.split("-").map((datePart: string) => Number.parseInt(datePart));
 
     if (year < 1900 || year > 2100) {
         return null;
@@ -17,6 +11,14 @@ export const formatDate = (dateToFormat: string): object | null => {
 
     if (day < 1 || day > 31) {
         return null;
+    }
+
+    if (day <= 31 && day > 28) {
+        const date = new Date(year, month - 1, day);
+
+        if (date.getDate() !== day) {
+            throw new Error("you are using the wrong day");
+        }
     }
 
     const result = { year, month, day };
